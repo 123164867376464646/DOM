3d301902f350cde9afe20cee51a8e57d306dce6a
linmumu <1054742101@qq.com>
2022-06-04 19:03:03 +0800

    1.0

---
+++ src/dom.js
@@ -0,0 +1,30 @@
+window.dom = {
+  style(node, name, value) {
+    if (arguments.length === 3) {
+      //dom.style(div, 'color', 'red')
+      node.style[name] = value;
+    } else if (arguments.length === 2) {
+      //dom.style(div,'color')
+      if (typeof name === 'string') {
+        return node.style[name];
+      } else if (name instanceof Object) {
+        //dom.style(div, {color: 'red'})
+        const object = name;
+        for (let key in object) {
+          // key: border / color
+          // node.style.border = ...
+          // node.style.color = ...
+          node.style[key] = object[key]
+        }
+      }
+    }
+  },
+  find(selector, scope){
+    return (scope || document).querySelectorAll(selector)
+  },
+   each(nodeList, fn){
+     for(let i = 0; i<nodeList.length; i++){
+      fn.call(null, nodeList[i])
+     }
+   }
+};
